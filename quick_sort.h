a57e1a759e67ed24519d166de3070e0505da9b4e
Lucas Antognoni <antognoni.lucas@gmail.com>
2016-11-17 14:19:16 -0200

    Quicksort

--- quick_sort.h
+++ quick_sort.h
@@ -3,17 +3,7 @@
 
 void swap(int *array, int i, int j);
 
-int random_pivot(int *array, int p, int r);
-
-int median(int array[]);
-
-int median_pivot(int *array, int p, int r);
-
-int middle_pivot(int *array, int p, int r);
-
-int last_pivot_partition(int *array, int p, int r);
-
-int first_pivot_partition(int *array, int p, int r);
+int partition(int *array, int p, int r);
 
 void quick_sort(int *array, int p, int r);
 
