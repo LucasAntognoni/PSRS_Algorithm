d226ce4e11588b4fa602e1334b1ce5e6cdb335fd
Victor Moneratto <victor.moneratto@gmail.com>
2016-11-21 21:28:35 -0200

    Implement phases 1 and 2

--- src/psrs.c
+++ src/psrs.c
@@ -10,50 +10,91 @@
 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "tools.h"
 #include "quick_sort.h"
+#include "tools.h"
 
-void Phase1(int* Elements, const int NumberOfElements, const int NumberOfProcs);
+int *FindSamples(int *Elements, const int NumberOfElements,
+                 const int NumberOfProcs, int *NumberOfSamples);
+int *FindPivots(int **Samples, int NumberOfProcs, int NumberOfSamples, int* NumberOfPivots);
 
-int main(int argc, char* argv[]) {
-    // Checking input arguments
-    if (argc < 3) {
-        printf("Use: %s <Number of Elements> <Number of Processes>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+int main(int argc, char *argv[]) {
+  // Checking input arguments
+  if (argc < 3) {
+    printf("Use: %s <Number of Elements> <Number of Processes>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    int NumberOfElements = atoi(argv[1]);
-    int NumberOfProcs = atoi(argv[2]);
-    const int MAX_ELEMENT = 1000;
+  int NumberOfElements = atoi(argv[1]);
+  int NumberOfProcs = atoi(argv[2]);
+  const int MAX_ELEMENT = 1000;
 
-    int* Elements = create_vector(NumberOfElements);
-    initialize_vector(Elements, NumberOfElements, MAX_ELEMENT);
+  //
+  // IMPORTANTE:
+  //
+  int *Elements = create_vector(NumberOfElements);
+  initialize_vector(Elements, NumberOfElements, MAX_ELEMENT);
 
-    // PHASE 1
-    Phase1(Elements, NumberOfElements, NumberOfProcs);
+  // Phases 1
+  int NumberOfSamples;
+  int *Samples =
+      FindSamples(Elements, NumberOfElements, NumberOfProcs, &NumberOfSamples);
 
-    // PHASE 2
+  // Phase 2
+  int NumberOfPivots;
+  int* Pivots = FindPivots(&Samples, NumberOfProcs, NumberOfSamples, &NumberOfPivots);
 
-    // PHASE 3
+  // Phase 3
 
-    // PHASE 4
+  // PHASE 4
 
-    print_vector(Elements, NumberOfElements);
-    destroy_vector(&Elements);
+  print_vector(Elements, NumberOfElements);
+  destroy_vector(&Elements);
 }
 
-void Phase1(int* Elements, const int NumberOfElements, const int NumberOfProcs) {
-    #pragma omp parallel num_threads(NumberOfProcs)
-    {
-        const int ProcessNumber = omp_get_thread_num();
-        const int Start = (ProcessNumber) * NumberOfElements / NumberOfProcs;
-        const int End = (ProcessNumber + 1) * NumberOfElements / NumberOfProcs;
-        quick_sort(Elements, Start, End);
+int *FindSamples(int *Elements, const int NumberOfElements,
+                 const int NumberOfProcs, int *NumberOfSamples) {
+  const int ProcsSqr = (NumberOfProcs * NumberOfProcs);
+
+  *NumberOfSamples = ProcsSqr;
+  int *Samples = (int *)malloc(*NumberOfSamples * sizeof(*Samples));
+
+#pragma omp parallel num_threads(NumberOfProcs)
+  {
+    const int ProcessNumber = omp_get_thread_num();
+    const int Start = (ProcessNumber)*NumberOfElements / NumberOfProcs;
+    const int End = (ProcessNumber+1) * NumberOfElements / NumberOfProcs;
+    quick_sort(Elements, Start, End - 1);
+
+    for (int iLocal = 0; iLocal < NumberOfProcs; iLocal++) {
+      const int iElement =
+          Start + (int)(iLocal * (NumberOfElements / (float)ProcsSqr));
+      const int iSample = (ProcessNumber * NumberOfProcs) + iLocal;
+      Samples[iSample] = Elements[iElement];
     }
+  }
+
+  return Samples;
+}
+
+int *FindPivots(int **Samples, int NumberOfProcs, int NumberOfSamples, int* NumberOfPivots) {
+
+  quick_sort(*Samples, 0, NumberOfSamples - 1);
+
+  *NumberOfPivots = NumberOfProcs - 1;
+  int *Pivots = (int *)malloc(*NumberOfPivots * sizeof(*Pivots));
+  for (int iPivot = 0; iPivot < *NumberOfPivots; ++iPivot) {
+    const int iSample = (iPivot+1) * NumberOfProcs + (NumberOfProcs / 2) - 1;
+    Pivots[iPivot] = *Samples[iSample]; 
+  }
+
+  free(*Samples);
+  *Samples = NULL;
+
+  return Pivots;
 }
